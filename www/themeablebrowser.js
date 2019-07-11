f71646648a13ad2ba2f30c2d20de71397029a8e9
David Duan <david.duan@fabricgroup.com.au>
2019-07-11 16:48:26 +0800

    add hide function

--- www/themeablebrowser.js
+++ www/themeablebrowser.js
@@ -48,6 +48,9 @@ ThemeableBrowser.prototype = {
         exec(null, null, 'ThemeableBrowser', 'show', []);
         return this;
     },
+    hide: function (eventname) {
+        exec(null, null, "ThemeableBrowser", "hide", []);
+    },
     reload: function (eventname) {
         exec(null, null, 'ThemeableBrowser', 'reload', []);
         return this;
